99d7a3a80b7de7fd773b69efff16cf4e3c0b3e77
hwfan <hwfan_bupt@outlook.com>
2020-09-13 00:11:53 +0800

    1.1

---
+++ lib/downloader/gd_helper.py
@@ -0,0 +1,42 @@
+# Author: Hongwei Fan(@hwfan)
+# Date: Dec 25, 2019
+# Last Update: Mar 23, 2020
+import requests
+import urllib.parse as urlparse
+import sys
+from lib.utils.content_processor import *
+from fake_useragent import UserAgent
+
+def gd_download(url, custom_filename, user_proxy):
+    ua = UserAgent(verify_ssl=False)
+
+    parsed_url = urlparse.urlparse(url)
+    id_str = urlparse.parse_qs(parsed_url.query)['id'][0]
+    URL = "https://drive.google.com/uc?export=download"
+    headers={'Accept-Encoding': '', 'User-Agent':ua.random, 'Host': 'google.com'}
+    session = requests.Session()
+    if user_proxy is not None:
+      response = session.get(URL, params = { 'id' : id_str }, stream = True, proxies={'http':user_proxy,'https':user_proxy}, headers=headers)
+    else:
+      response = session.get(URL, params = { 'id' : id_str }, stream = True, headers=headers)
+    ipdb.set_trace()
+    token = get_confirm_token(response)
+    if token:
+        params = { 'id' : id_str, 'confirm' : token }
+        # if user_proxy is not None:
+        response = session.get(URL, params = params, stream = True, proxies={'http':user_proxy,'https':user_proxy},headers=headers)
+        # else:
+        #   response = session.get(URL, params = params, stream = True, headers=headers)
+    filename_parsed, filesize = parse_response_header(response)
+    filename = filename_parsed if len(custom_filename) == 0 else custom_filename
+    save_response_content(response, filename, filesize)    
+    
+
+if __name__ == '__main__':
+  print('============ GoogleDrive Direct Downloader - Debug Mode ============')
+  original_url = sys.argv[1]
+  filename = sys.argv[2].strip()
+  user_proxy = 'socks5h://127.0.0.1:1080'
+  if len(sys.argv) > 3:
+    user_proxy = sys.argv[3]
+  gd_download(original_url, filename, user_proxy)
---
+++ lib/downloader/od_helper.py
@@ -0,0 +1,32 @@
+# Author: Hongwei Fan(@hwfan)
+# Date: Dec 25, 2019
+# Last Update: Mar 23, 2020
+import requests
+import urllib.parse as urlparse
+import sys
+from lib.utils.content_processor import *
+
+def od_download(url, custom_filename, user_proxy):
+    headers={'Accept-Encoding': ''}
+    session = requests.Session()
+    
+    parsed_url = urlparse.urlparse(url)
+    replaced_parsed = parsed_url._replace(netloc='1drv.ws')
+    replaced_url = urlparse.urlunparse(replaced_parsed)
+    if user_proxy is not None:
+      response = session.get(replaced_url, stream = True, proxies={"http":user_proxy,"https":user_proxy}, headers=headers)
+    else:
+      response = session.get(replaced_url, stream = True, headers=headers)
+
+    filename_parsed, filesize = parse_response_header(response)
+    filename = filename_parsed if len(custom_filename) == 0 else custom_filename
+    save_response_content(response, filename, filesize)
+    
+if __name__ == '__main__':
+  print('============ OneDrive Direct Downloader - Debug Mode ============')
+  url = sys.argv[1]
+  filename = sys.argv[2].strip()
+  user_proxy = 'socks5h://127.0.0.1:1080'
+  if len(sys.argv) > 3:
+    user_proxy = sys.argv[3]
+  od_download(url, filename, user_proxy)
